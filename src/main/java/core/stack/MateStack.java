6791cff9d8914810b085a698ae332b07381a8c02
Brynkiewicz <michal.brynkiewicz@envistaco.com>
2026-04-23 21:50:00 +0200

    first solution

--- src/main/java/core/stack/MateStack.java
+++ src/main/java/core/stack/MateStack.java
@@ -1,19 +1,52 @@
 package core.stack;
 
-public class MateStack<T> {
-    public void push(T value) {
+import java.util.*;
 
+public class MateStack<T> {
+    private int size = 0;
+    private Object[] stack = new Object[10];
+
+    public void push(T value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value cannot be null");
+        }
+
+        if (size == stack.length) {
+            resize();
+        }
+
+        stack[size++] = value;
     }
 
     public T peek() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+
+        return (T) stack[size - 1];
     }
 
     public T pop() {
-        return null;
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+
+        T value = (T) stack[--size];
+        stack[size] = null;
+        return value;
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    public void resize() {
+        Object[] newStack = new Object[stack.length + 10];
+        System.arraycopy(stack, 0, newStack, 0, size);
+        stack = newStack;
     }
 }
